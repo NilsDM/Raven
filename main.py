12b85082458b51c82bb286f782c454cbe42a1bee
NilsDM <nilsdmikkelsen@gmail.com>
2021-03-23 19:37:49 -0700

    end of day one

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-import webbrowser
+import webbrowser as wb
+import time
 import requests
 import bs4
 import selenium
 
 
 def main():
-    print("Hello world")
+    wb.open("https://www.reddit.com/r/stocks")
+    # time.sleep()
+    res = requests.get("https://www.reddit.com/r/stocks")
+    # print(requests.status_codes)
+    # res.raise_for_status()
+    print(len(res.text))
+    print(res.text[:2500])
 
 
 if __name__ == "__main__":
