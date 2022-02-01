4accd4bc0e90482c3cd919e1783353bb1bc2ab1f
NilsDM <Nilsdosaj@gmail.com>
2022-02-01 00:14:59 -0800

    cleanup

---
+++ python_scraper_files/main.py
@@ -0,0 +1,16 @@
+
+import reddit_tractor_beam as rtb
+import stock_name_tractor_beam as stb
+import comment_blender as cb
+
+
+def main():
+    reddit_data = rtb.build_reddit_data_frame() # Scrape reddit
+    # stock_data = stb.stock_name_parser()
+    # comment_data = cb.analyse(reddit_data, stock_data)
+
+    print("okay")
+
+
+if __name__ == "__main__":
+    main()
